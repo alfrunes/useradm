eb4887669c74c75a4043ab8127cd465b737ff18c
Alf-Rune Siqveland <alf.rune@northern.tech>
2022-10-26 16:31:52 +0200

    fix: User updates that makes comparisons ensure atomicity
    This commit introduces entity tags under the hood without exposing it to
    the API and deals with ensuring that concurrent updates to a single user
    is either atomic or fails.
    
    Changelog: Title
    Ticket: None
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- api/http/api_useradm.go
+++ api/http/api_useradm.go
@@ -1,16 +1,16 @@
 // Copyright 2022 Northern.tech AS
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//	    http://www.apache.org/licenses/LICENSE-2.0
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 package http
 
 import (
@@ -60,6 +60,8 @@ const (
 	defaultTimeout = time.Second * 5
 	hdrETag        = "ETag"
 	hdrIfMatch     = "If-Match"
+
+	pathParamMe = "me"
 )
 
 const (
@@ -359,7 +361,7 @@ func (u *UserAdmApiHandlers) GetUserHandler(w rest.ResponseWriter, r *rest.Reque
 
 func (u *UserAdmApiHandlers) UpdateUserHandler(w rest.ResponseWriter, r *rest.Request) {
 	ctx := r.Context()
-
+	idty := identity.FromContext(ctx)
 	l := log.FromContext(ctx)
 
 	userUpdate, err := parseUserUpdate(r)
@@ -383,6 +385,9 @@ func (u *UserAdmApiHandlers) UpdateUserHandler(w rest.ResponseWriter, r *rest.Re
 	}
 
 	id := r.PathParam("id")
+	if strings.EqualFold(id, "me") {
+		id = idty.Subject
+	}
 	err = u.userAdm.UpdateUser(ctx, id, userUpdate)
 	if err != nil {
 		switch err {
